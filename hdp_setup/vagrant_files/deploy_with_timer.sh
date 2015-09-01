5350f15d32153972c36ec3d26579675b0c6c2ab2
amin-amirali <amin.amirali@teradata.com>
2015-09-01 13:40:15 +0200

    minor changes

--- hdp_setup/vagrant_files/deploy_with_timer.sh
+++ hdp_setup/vagrant_files/deploy_with_timer.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo -e "\n\n starting deploy at `date` \n\n" >> timer.out
+echo -e "Starting deploy at `date`\n" >> timer.out
 echo -e "----------------------------------\n"
 ./deploy.sh
 echo -e "----------------------------------\n"
 echo -e "----------------------------------\n"
-echo -e "finished deploy at `date` \n\n" >> timer.out
+echo -e "finished deploy at `date`\n" >> timer.out
 
